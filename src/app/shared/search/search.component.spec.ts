72ab9c3c1974d24e7071401825e4043545974e42
William Gregory <William.Gregory@talktalkplc.com>
2018-04-29 23:58:26 +0100

    Updating test coverage

--- src/app/shared/search/search.component.spec.ts
+++ src/app/shared/search/search.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { SearchComponent } from './search.component';
@@ -26,5 +27,44 @@ describe('SearchComponent', () => {
     expect(fixture).toMatchSnapshot();
   });
 
+  describe('keywords', () => {
 
+    it('should add a keyword', () => {
+      component.keyword = 'updated value';
+      component.addKeyword();
+
+      fixture.detectChanges();
+
+      expect(component.keywords).toEqual(['updated value']);
+    });
+
+    it('should remove a keyword', () => {
+      component.keywords = ['remove'];
+
+      fixture.detectChanges();
+
+      expect(component.keywords).toEqual(['remove']);
+
+      component.removeKeyword('remove');
+
+      fixture.detectChanges();
+
+      expect(component.keywords).toEqual([]);
+    });
+
+    it('should clear all keywords', () => {
+      component.keywords = ['remove', 'all', 'words'];
+
+      fixture.detectChanges();
+
+      expect(component.keywords).toEqual(['remove', 'all', 'words']);
+
+      component.clear();
+
+      fixture.detectChanges();
+
+      expect(component.keywords).toEqual([]);
+    });
+
+  });
 });
